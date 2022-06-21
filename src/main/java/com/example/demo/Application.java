546b5e0de91661f7e138ee397d6823ebddb5246a
Eric Straw <estraw@smu.edu>
2022-06-20 19:28:37 -0500

    Finished Spring JPA Tutorial

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student eric = new Student(
+                    "Eric",
+                    "Straw",
+                    "estraw@smu.edu",
+                    26
+                    );
+            studentRepository.save(eric);
+        };
     }
 
 }
